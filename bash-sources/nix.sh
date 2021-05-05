d3e09de9b02d2f37734ea266af31785d7cfbe29d
James Johnson <d0c.s4vage@gmail.com>
2021-05-05 10:58:33 -0700

    Fixed color escaping

--- bash-sources/nix.sh
+++ bash-sources/nix.sh
@@ -1,14 +1,14 @@
 function nix_prompt {
 	rc=$?
 	if [ "$IN_NIX_SHELL" ] ; then
-	    main_color="\e[1;34m"
+	    main_color="\[\e[1;34m\]"
 	    main_prefix="‖nix‖ "
 	else
 	    main_color="\[\e[1;30m\]"
 	    main_prefix=""
 	fi
 	if [ $rc -ne 0 ] ; then
-		rc_disp="\e[1;31m\][\\\$?=$rc]${main_color}"
+		rc_disp="\[\e[1;31m\][\\\$?=$rc]${main_color}"
 	else
 		rc_disp="[\\\$?=0]"
 	fi
